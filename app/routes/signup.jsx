bea02d6320917dca8485b20ae3fea44663a0efda
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-04 13:44:21 +0100

    added styling for signup

--- app/routes/signup.jsx
+++ app/routes/signup.jsx
@@ -1,11 +1,16 @@
 import { Form } from "@remix-run/react";
+import { useState } from "react";
 
 export default function SignUp() {
+
   return (
-    <div>
-      <Form className="flex flex-col" id="sign-up-form" method="post">
+    <div className="flex justify-center items-center w-full">
+      <Form className="flex flex-col bg-white shadow-md p-4 rounded-xl w-full" id="sign-up-form" method="post">
+        <h1 className="mb-4 pb-3 border-b-2 border-black font-bold text-xl">Sign Up</h1>
+        <div className="flex mb-4 w-full">
+      <div className="w-full">
         <label htmlFor="firstname">Firstname</label>
-        <input
+        <input className="border-2 border-gray-700 p-1 rounded w-[95%]"
           id="firstname"
           type="text"
           name="firstname"
@@ -13,8 +18,11 @@ export default function SignUp() {
           placeholder="Type your firstname..."
           autoComplete="off"
         />
+        </div>
+        <div className="w-full">
         <label htmlFor="lastname">Lastname</label>
-        <input
+       
+        <input className="border-2 border-gray-700 p-1 rounded w-[95%]"
           id="lastname"
           type="text"
           name="lastname"
@@ -22,8 +30,39 @@ export default function SignUp() {
           placeholder="Type your lastname..."
           autoComplete="off"
         />
+        </div>
+        </div>
+
+        <div className="flex flex-col mb-4">
+        <label htmlFor="mail">Mail</label>
+        <input className="border-2 border-gray-700 p-1 rounded w-full"
+          id="mail"
+          type="email"
+          name="mail"
+          aria-label="mail"
+          placeholder="Type your mail..."
+          required
+          autoComplete="off"
+        />
+          </div>
+        <div className="flex flex-col mb-4">
+        <label htmlFor="password">Password</label>
+
+
+        <input
+        className="border-2 border-gray-700 p-1 rounded w-full"
+          id="password"
+          type="password"
+          name="password"
+          aria-label="password"
+          placeholder="Type your password..."
+        />
+        </div>
+
+<div className="flex flex-col mb-4">
         <label htmlFor="address">Address</label>
         <input
+        className="border-2 border-gray-700 p-1 rounded w-full"
           id="address"
           type="text"
           name="address"
@@ -32,8 +71,13 @@ export default function SignUp() {
           autoComplete="off"
         />
 
+        </div>
+
+        <div className="flex flex-col mb-4">
+
         <label htmlFor="languages">Languages</label>
         <input
+        className="border-2 border-gray-700 p-1 rounded w-full"
           id="languages"
           type="text"
           name="languages"
@@ -42,7 +86,9 @@ export default function SignUp() {
           autoComplete="off"
         />
 
-        <fieldset>
+</div>
+
+        <fieldset className="flex justify-around mb-4">
           <legend>Gender:</legend>
           <input type="radio" id="male" name="gender" value="Male" />
           <label htmlFor="male">Male</label>
@@ -53,28 +99,8 @@ export default function SignUp() {
           <input type="radio" id="other" name="gender" value="Other" />
           <label htmlFor="other">Other</label>
         </fieldset>
-
-        <label htmlFor="mail">Mail</label>
-        <input
-          id="mail"
-          type="email"
-          name="mail"
-          aria-label="mail"
-          placeholder="Type your mail..."
-          required
-          autoComplete="off"
-        />
-        <label htmlFor="password">Password</label>
-
-        <input
-          id="password"
-          type="password"
-          name="password"
-          aria-label="password"
-          placeholder="Type your password..."
-        />
-        <div className="btns">
-          <button type="submit">Sign Up</button>
+        <div className="mt-2 w-full text-white">
+          <button className="bg-[#333] p-2 rounded w-full text-lg" type="submit">Sign Up</button>
         </div>
       </Form>
     </div>
