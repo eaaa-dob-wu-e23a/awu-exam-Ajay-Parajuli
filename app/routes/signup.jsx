b328abb597558a794ad63a96028ce038450e6277
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-04 22:00:42 +0100

    styled error message - they will show one error at a time

--- app/routes/signup.jsx
+++ app/routes/signup.jsx
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 
 export default function SignUp() {
 const actionData = useActionData();
+const [currentErrorIndex, setCurrentErrorIndex] = useState(0); // State to track the current error index
+
 console.log(actionData);
 
 
@@ -13,6 +15,15 @@ console.log(actionData);
             <h1 className="mt-2 font-bold text-3xl">GetFit</h1>
       <Form className="flex flex-col shadow-2xl p-4 rounded-xl w-[95%] sm:w-[85%] md:w-[70%] lg:w-[60%] xl:w-[40%] 2xl:w-[30%]" id="sign-up-form" method="post">
         <h2 className="border-gray-300 mb-4 pb-3 border-b-2 font-medium text-xl">Sign Up</h2>
+        <div>
+        <div className="bg-red-500 mb-3 rounded w-full text-center">
+          {actionData?.errors && Object.keys(actionData.errors).length > 0 && (
+            <p className="p-1 text-white">
+              {Object.values(actionData.errors)[currentErrorIndex].message}
+            </p>
+          )}
+        </div>
+        </div>
         <div className="flex justify-between mb-4 w-full">
       <div className="w-[100%]">
         <label htmlFor="firstname"><span className="block after:content-['*'] after:ml-0.5 font-medium text-slate-700 text-sm after:text-red-500">
@@ -27,12 +38,6 @@ console.log(actionData);
           autoComplete="off"
         />
         </div>
-
-        {actionData?.errors.firstname && (
-          <p className="mt-1 mb-0 text-red-500">
-            {actionData.errors.firstname.message}
-          </p>
-        )}
         <div className="w-[100%]">
 
         <label className="bg-black w-full" htmlFor="lastname"><span className="block after:content-['*'] after:ml-0.5 font-medium text-slate-700 text-sm after:text-red-500">
@@ -54,20 +59,23 @@ console.log(actionData);
   
         </div>
 
+      
+
         <div className="flex flex-col mb-4">
         <label htmlFor="mail"> <span className="block after:content-['*'] after:ml-0.5 font-medium text-slate-700 text-sm after:text-red-500">
     Email
   </span></label>
         <input className="border-2 border-gray-300 p-1 rounded w-full"
           id="mail"
-          type="email"
+          type="text"
+
           name="mail"
           aria-label="mail"
           placeholder="Type your mail..."
-          required
           autoComplete="off"
         />
           </div>
+    
         <div className="flex flex-col mb-4">
         <label htmlFor="password"><span className="block after:content-['*'] after:ml-0.5 font-medium text-slate-700 text-sm after:text-red-500">
     Password
@@ -83,11 +91,7 @@ console.log(actionData);
           placeholder="Type your password..."
         />
         </div>
-        {actionData?.errors.password && (
-          <p className="mt-1 mb-0 text-red-500">
-            {actionData.errors.password.message}
-          </p>
-        )}
+      
 
 <div className="flex flex-col mb-4">
         <label className="font-medium text-slate-700 text-sm" htmlFor="address">Address</label>
