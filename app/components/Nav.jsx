03b9c2a54daacd8f204706ae73913abcb3aafd0a
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-04 12:00:59 +0100

    added relavant links to nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,6 +2,11 @@ import { NavLink } from "@remix-run/react";
 
 export default function Nav() {
   return (
- <></>
+    <nav>
+    <NavLink to="/posts">Events</NavLink>
+    <NavLink to="/create-event">Create Event</NavLink>
+    <NavLink to="/Your Events">Your Events</NavLink>
+    <NavLink to="/profile">Profile</NavLink>
+  </nav>
   );
 }
