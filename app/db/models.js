06e7f1427718b7ac6b97486d1e13c40464c5325c
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-04 10:34:50 +0100

    created eventSchema

--- app/db/models.js
+++ app/db/models.js
@@ -19,12 +19,40 @@ const userSchema = new Schema(
       required: true, // Ensure user passwords are required
       select: false // Automatically exclude from query results
     },
-    registered_events: [String], 
+    registered_events: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Event'
+    }], 
     languages: [String],
     address: String,   
   },
   { timestamps: true }  // Automatically include createdAt and updatedAt fields
 );
+
+const eventSchema = new Schema(
+  {
+    image: String,
+    title: {
+      type: String,
+      required: true, // Ensure event titles are required
+      unique: true // Ensure event titles are unique
+    },
+    description: String,
+    date: Date,
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    participants: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+  }], 
+    maxParticipants: Number,
+    address: String,
+    organizer: String,   
+  },
+  { timestamps: true }  // Automatically include createdAt and updatedAt fields
+ );
 
 
 export const models = [
@@ -32,5 +60,10 @@ export const models = [
     name: "User",
     schema: userSchema,
     collection: "users",
+  },
+  {
+    name: "Event",
+    schema: eventSchema,
+    collection: "events",
   },
 ];
