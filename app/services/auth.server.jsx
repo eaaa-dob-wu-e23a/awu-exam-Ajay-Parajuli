39ddee2f46b6b4bcfdf9b3792a1d6bd00d6836ec
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 08:19:13 +0100

    login validation created by retriving error messages from sessionstorage

--- app/services/auth.server.jsx
+++ app/services/auth.server.jsx
@@ -1,58 +1,53 @@
-// app/services/auth.server.ts
 import { Authenticator, AuthorizationError } from "remix-auth";
 import { sessionStorage } from "./session.server";
 import { FormStrategy } from "remix-auth-form";
-import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-
-
+import mongoose from "mongoose";
 
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
-export let authenticator = new Authenticator(sessionStorage, { // 
-    sessionErrorKey: "sessionErrorKey" // keep in sync
-  });
+export let authenticator = new Authenticator(sessionStorage, {
+  sessionErrorKey: "sessionErrorKey" // keep in sync
+});
 
-  async function verifyUser({ mail, password }) {
-    const user = await mongoose.models.User.findOne({ mail }).select("+password");
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!user) {
-      throw new AuthorizationError("No user found with this email.");
+// Tell the Authenticator to use the form strategy
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    let mail = form.get("mail");
+    let password = form.get("password");
+
+    // do some validation, errors are saved in the sessionErrorKey
+    if (!mail || typeof mail !== "string" || !mail.trim()) {
+      throw new AuthorizationError("Enter a valid email address.");
     }
-    if (!passwordMatch) {
-      throw new AuthorizationError("Invalid password.");
+
+    if (!password || typeof password !== "string" || !password.trim()) {
+      throw new AuthorizationError("Enter a valid password.");
     }
-    user.password = undefined; 
-    return user;
-  }
 
-
-  authenticator.use(
-    new FormStrategy(async ({ form }) => {
-      let mail = form.get("mail");
-      let password = form.get("password");
-      const user = await verifyUser({ mail, password });
-
-      if (!mail || mail?.length === 0) {
-        throw new AuthorizationError("Bad Credentials: Email is required");
-      }
-      if (typeof mail !== "string") {
-        throw new AuthorizationError("Bad Credentials: Email must be a string");
-      }
-  
-      if (!password || password?.length === 0) {
-        throw new AuthorizationError("Bad Credentials: Password is required");
-      }
-      if (typeof password !== "string") {
-        throw new AuthorizationError("Bad Credentials: Password must be a string");
-      } 
-  
+    // verify the user
+    const user = await verifyUser({ mail, password });
     if (!user) {
       // if problem with user throw error AuthorizationError
-      throw new AuthorizationError("Bad Credentials: User not found ");
+      throw new AuthorizationError("User not found");
     }
+    console.log(user);
     return user;
-        } ,
-      ),
-    "user-pass" // 
-  );
+  }),
+  "user-pass"
+);
+
+async function verifyUser({ mail, password }) {
+  const user = await mongoose.models.User.findOne({ mail }).select("+password");
+  if (!user) {
+    throw new AuthorizationError("No user found with this email.");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    throw new AuthorizationError("Invalid password.");
+  }
+  // Remove the password from the user object before returning it
+  user.password = undefined;
+  return user;
+}
