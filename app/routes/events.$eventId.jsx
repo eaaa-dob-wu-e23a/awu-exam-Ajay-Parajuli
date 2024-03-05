822f599dcaba02709415eb100de09dc8f7361187
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 19:00:22 +0100

    added cover event photo

--- app/routes/events.$eventId.jsx
+++ app/routes/events.$eventId.jsx
@@ -27,6 +27,34 @@ export const meta = () => {
 export default function Event() {
     const { event, authUser, users } = useLoaderData();
     return(
+      <>
+      <div>
+        <div className="relative">
+          <img className=""  src={event.image} alt={event.description} />
+          <h1 className="top-1/2 left-1/2 absolute bg-white bg-opacity-75 rounded w-[90%] font-medium text-center text-lg transform -translate-x-1/2 -translate-y-1/2">{event.description}</h1>
+          </div>
+          <div className="flex mb-2 p-2">
+        <img className="rounded-full w-[50px] h-[50px] object-cover" src={event.created_by.image} alt={event.created_by.firstname} />
+        <span className="ml-2">
+          <h3 className="font-medium text-black">{event.created_by.firstname} {event.created_by.lastname}</h3>
+          <p>{event.created_by.mail}</p>
+        </span>
+      </div>
+      <div className="p-2">
+        <h3 className="font-medium">About me</h3>
+        <p className="text-gray-500">Languages: {event.created_by.languages.map((language, index) => (
+            <span key={language}>
+                {language}{index !== event.created_by.languages.length - 1 ? ', ' : ''}
+            </span>
+        ))}</p>
+        <p className="text-gray-500">Age: {event.created_by.age}</p>
+      </div>
+
+          <p>{event.date}</p>
+          <p>{event.address}</p>
+          <p>{event.maxParticipants}</p>
+          <p>{event.organizer}</p>
+      </div>
         <div className="text-black">
             {users.map((user) => (
                 <div key={user._id}>
@@ -35,5 +63,6 @@ export default function Event() {
             ))
             }
         </div>
+        </>   
     );
 }
