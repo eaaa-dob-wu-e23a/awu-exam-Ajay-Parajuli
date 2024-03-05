24caddf27ec50a3fd756e8d1b0f955c4b8409f9f
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 09:29:04 +0100

    export defalut profile for routes to work

---
+++ app/routes/profile.jsx
@@ -0,0 +1,20 @@
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { authenticator } from "../services/auth.server";
+
+import mongoose from "mongoose";
+import { redirect } from "@remix-run/node";
+
+
+
+
+export default function Profile() {
+
+
+  
+  return (
+    <div>
+
+    </div>
+  );
+}
+
