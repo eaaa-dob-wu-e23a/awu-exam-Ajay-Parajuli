11b93b90d3aefb48c0bc4cb08d719ab5360284e7
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 19:41:10 +0100

    fully styled detailed page but needs to be responsive

--- app/routes/events.$eventId.jsx
+++ app/routes/events.$eventId.jsx
@@ -40,14 +40,14 @@ export default function Event() {
           <img className=""  src={event.image} alt={event.description} />
           <h1 className="top-1/2 left-1/2 absolute bg-white bg-opacity-75 rounded w-[90%] font-medium text-center text-lg transform -translate-x-1/2 -translate-y-1/2">{event.description}</h1>
           </div>
-          <div className="flex mb-2 p-2">
+          <div className="flex p-2">
         <img className="rounded-full w-[50px] h-[50px] object-cover" src={event.created_by.image} alt={event.created_by.firstname} />
         <span className="ml-2">
           <h3 className="font-medium text-black">{event.created_by.firstname} {event.created_by.lastname}</h3>
           <p>{event.created_by.mail}</p>
         </span>
       </div>
-      <div className="p-2">
+      <div className="p-2 leading-7">
         <h3 className="mb-1 font-medium text-lg">About me</h3>
         <p className="text-gray-500">Languages: {event.created_by.languages.map((language, index) => (
             <span key={language}>
@@ -57,26 +57,38 @@ export default function Event() {
         <p className="text-gray-500">Age: {event.created_by.age}</p>
         <p className="text-gray-500">Gender: {event.created_by.gender}</p>
       </div>
-      <div className="p-2">
+      <div className="pl-2 leading-6">
       <h3 className="font-medium">Bio</h3>
 
       <p className="text-gray-500">About: {event.created_by.about}</p>
 
       </div>
 
-<div className="p-2">
+<div className="p-2 leading-7">
 <h3 className="mb-1 font-medium text-lg">About the Event</h3>
 
 <p className="text-gray-500">Event date: {formatDate(event.date)}</p>
-          <p>Event address: {event.address}</p>
-          <p>{event.maxParticipants}</p>
-          <p>{event.organizer}</p>
+<p className="text-gray-500">Event address: {event.address}</p>
+<p className="text-gray-500">Available slots: {event.maxParticipants}</p>
+
+<p className="text-gray-500">Event organizer: {event.organizer}</p>
+
           </div>
+
+          <div className="pl-2">
+      <h3 className="font-medium">Description:</h3>
+
+      <p className="text-gray-500">{event.created_by.about}</p>
+
       </div>
-        <div className="text-black">
+      </div>
+      <h3 className="mb-1 p-2 font-medium text-lg">People who are going</h3>
+
+        <div className="flex flex-wrap justify-around p-2 w-full text-black">
             {users.map((user) => (
-                <div key={user._id}>
-                    <p>{user.firstname}</p>
+                <div className="" key={user._id}>
+                    <img className="rounded-full w-[50px] h-[50px] object-cover" src={user.image} alt={event.created_by.firstname} />
+                    <p className="text-center">{user.firstname}</p>
                 </div>
             ))
             }
