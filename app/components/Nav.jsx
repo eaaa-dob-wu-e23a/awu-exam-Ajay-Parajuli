061ba00a4809d7ae0dbefc6d0b0e772cd472ba63
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 13:01:52 +0100

    change name

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "@remix-run/react";
 export default function Nav() {
   return (
     <nav className="flex justify-around items-center bg-black hover:shadow-lg w-full h-[80px] font-med font-medium text-white">
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/posts">Events</NavLink>
+    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/events">Events</NavLink>
     <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/create-event">Create</NavLink>
     <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/your-events">My events</NavLink>
     <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/profile">Profile</NavLink>
