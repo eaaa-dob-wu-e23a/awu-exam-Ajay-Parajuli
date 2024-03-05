68c417d5171af3077842a7207a43c439ba837b8d
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 13:47:39 +0100

    protected the loader route and fetched posts and populated user

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -4,10 +4,10 @@ export default function EventCard({ post }) {
   // Check if post exists and if post.image is not null
   return (
     <article className="post-card">
-      <UserAvatar user={post.user} />
+      <UserAvatar user={post.created_by} />
       <img src={post.image} alt={post.description} />
       <h3>{post.description}</h3>
-      <p>Max: {post.maxPaticipants}</p>
+      <p>Max: {post.maxParticipants}</p>
       <p>Address: {post.address}</p>
     </article>
   );
--- app/routes/events.jsx
+++ app/routes/events.jsx
@@ -1,20 +1,43 @@
 import EventCard from "../components/EventCard";
 import mongoose from "mongoose";
-
-
+import {authenticator} from "../services/auth.server";
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
 
 
 export const meta = () => {
     return [{ title: "GetFit app" }];
   };
 
+  export async function loader({ request }) {
 
+    await authenticator.isAuthenticated(request, {
+      failureRedirect: "/signin",
+    });
+  
+    try {
+      const posts = await mongoose.models.Event.find().populate("created_by").sort({ createdAt: -1 });
+      console.log(posts);
+      return json({ posts });
+    } catch (error) {
+      console.error(error);
+      throw new Response("Internal Server Error", { status: 500 });
+    }
+  }
+  
 
 
 export default function Events() {
+    const { posts } = useLoaderData();
     return (
         <div>
-        <h1>Events</h1>
+         <section className="grid">
+        {posts.map(post => (
+          <Link key={post._id} className="post-link" to={`${post._id}`}>
+            <EventCard post={post} />
+          </Link>
+        ))}
+      </section>
         </div>
     );
 }
