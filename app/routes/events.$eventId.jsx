32ba049ce4bf3d14b44e1756dd77484164ef19a9
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 16:57:59 +0100

    added loader function that protects the route and fetches specifik post using params

--- app/routes/events.$eventId.jsx
+++ app/routes/events.$eventId.jsx
@@ -1,9 +1,21 @@
+import { authenticator } from "../services/auth.server";
+import mongoose from "mongoose";
+import { json } from "@remix-run/node";
+
 
 export const meta = () => {
     return [{ title: "GetFit app" }];
   };
 
-
+  export async function loader({ request, params }) {
+    // Ensure the user is authenticated
+    const authUser = await authenticator.isAuthenticated(request, {
+      failureRedirect: "/signin",
+    });
+    // Load the post and the user who created it
+    const post = await mongoose.models.Post.findById(params.postId).populate("user");
+    return json({ post, authUser });
+  }
 
 
 export default function Event() {
