c61bfc824c0637a4e42a375242f477d8e70390d5
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 12:26:22 +0100

    change nav link  names

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,8 +4,8 @@ export default function Nav() {
   return (
     <nav className="flex justify-around items-center bg-black hover:shadow-lg w-full h-[80px] font-med font-medium text-white">
     <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/posts">Events</NavLink>
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/create-event">Create Event</NavLink>
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/your-events">Your Events</NavLink>
+    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/create-event">Create</NavLink>
+    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/your-events">My events</NavLink>
     <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/profile">Profile</NavLink>
   </nav>
   );
