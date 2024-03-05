3ca135430a9be0678d16eaf106768846f31a650b
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-05 09:32:53 +0100

    fethced user data from using moogoose

--- app/routes/profile.jsx
+++ app/routes/profile.jsx
@@ -5,15 +5,27 @@ import mongoose from "mongoose";
 import { redirect } from "@remix-run/node";
 
 
+export const meta = () => {
+    return [{ title: "GetFit app" }];
+    
+}
+
+export async function loader({ request }) {
+    const user = await authenticator.isAuthenticated(request, {
+      failureRedirect: "/signin"
+    });
+    const updatedUser = await mongoose.models.User.findById(user._id);
+    return { user: updatedUser };
+  }
+
+
 
 
 export default function Profile() {
 
-
-  
   return (
     <div>
-
+<h2>sdsd</h2>
     </div>
   );
 }
