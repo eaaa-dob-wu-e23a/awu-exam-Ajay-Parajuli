4d72ef6547cec279591edbbcf0aad948d2e87b40
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-06 18:08:39 +0100

    added event update file

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -7,7 +7,7 @@ export default function EventCard({ post }) {
   <UserAvatar user={post.created_by} />
   <div className="relative">
     <img className="rounded-md" src={post.image} alt={post.description} />
-    <div className="bottom-0 left-0 absolute bg-white bg-opacity-75 p-2">
+    <div className="bottom-0 left-0 absolute bg-white bg-opacity-75 p-2 w-[80%] rounded">
       <h3 className="font-medium">{post.description}</h3>
       <p>Available slots: {post.maxParticipants}</p>
       <p>Event at: {post.address}</p>
