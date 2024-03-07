ad63c1faf3242278beb23c554d6db7e1a8007925
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-07 21:58:02 +0100

    styled nav a bit different

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,11 +2,16 @@ import { NavLink } from "@remix-run/react";
 
 export default function Nav() {
   return (
-    <nav className="flex justify-around items-center bg-black hover:shadow-lg w-full h-[80px] font-med font-medium text-white">
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/events">Events</NavLink>
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/create-event">Create</NavLink>
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/your-events">My events</NavLink>
-    <NavLink className="flex justify-center items-center hover:bg-white m-auto w-full h-full hover:text-black cursor-pointer" to="/profile">Profile</NavLink>
-  </nav>
+    <nav className="fixed inset-x-0 top-0 flex justify-around items-center bg-black hover:shadow-lg h-16 font-medium text-white z-50">
+      <NavLink className="hover:bg-white w-full  flex justify-center hover:text-black cursor-pointer px-4 py-2" to="/events">
+        Events
+      </NavLink>
+      <NavLink className="hover:bg-white w-full flex justify-center hover:text-black cursor-pointer px-4 py-2" to="/create-event">
+        Create
+      </NavLink>
+      <NavLink className="hover:bg-white w-full flex justify-center hover:text-black cursor-pointer px-4 py-2" to="/profile">
+        Profile
+      </NavLink>
+    </nav>
   );
 }
